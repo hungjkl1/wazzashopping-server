61687bca2d08b5fbe7df28be0098604dc3926717
dangtrinhmanhhung <hungdtm@evolableasia.vn>
2019-09-06 16:50:28 +0700

    WS-002 parse-server config

--- parse-config/parse-server.js
+++ parse-config/parse-server.js
@@ -7,10 +7,17 @@ console.log('[i] - Server is on ' + serverURL)
 |--------------------------------------------------
 */
 const parseServerOption = {
-  databaseURI: ""
+  cloud: './cloud/main.js',
+  serverURL: serverURL,
+  appId: process.env.APPID,
+  masterKey: process.env.MASTER_KEY,
+  javascriptKey: process.env.JS_KEY,
+  restAPIKey: process.RESTAPI_KEY,
+  clientKey: process.CLIENT_KEY
 }
 /**
 |--------------------------------------------------
 | PARSE CONFIG--
 |--------------------------------------------------
 */
+module.exports = parseServerOption
